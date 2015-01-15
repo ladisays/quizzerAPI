86744e4bfca00866544159352bfbae3826284c96
andela-ladenusi <ladi.adenusi@andela.co>
2015-01-15 01:55:00 +0100

    Added mongolabs database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // config/database.js
 module.exports = {
-  'url' : 'mongodb://localhost/quizzerAPI'
+  // 'url' : 'mongodb://localhost/quizzerAPI'
+  'url' : 'mongodb://quizzerapi:quizzer@ds031641.mongolab.com:31641/quizzerapi'
 };
