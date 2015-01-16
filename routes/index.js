c5824392d7e4f4d6d04be60ca86d78a1c1338b74
andela-ladenusi <ladi.adenusi@andela.co>
2015-01-16 12:03:39 +0100

    Redid redirection in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -295,7 +295,7 @@ module.exports = function(router, passport) {
 
     // if they aren't redirect them to the home page
     // response.redirect('/');
-      response.send(401).redirect('/login');
+      response.status(401).redirect('/login');
     }
   }
 };
