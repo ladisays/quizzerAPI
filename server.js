9454fa050f7da7addda06f5ec50ed71f00661efc
andela-ladenusi <ladi.adenusi@andela.co>
2015-01-16 13:48:20 +0100

    Installed npm cors package

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // =============================================================================
 var express  = require('express');
 var app      = express();
+var cors     = require('cors');
 var router   = express.Router();
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
@@ -14,14 +15,16 @@ var session      = require('express-session');
 var configDB     = require('./config/database');
 
 // CORS configuration ==========================================================
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  next();
-};
+// var allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
+//   next();
+// };
 
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
+
+app.use(cors());
 
 // APP CONFIGURATION ===========================================================
 mongoose.connect(configDB.url); // connect to our database
